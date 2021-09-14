d2163c06f16e33f143c1442971b1f92d62df8348
Alan Toledo <alantoledo.work@gmail.com>
2021-09-14 20:52:37 -0300

    fix import error

--- src/components/AppWrapper/index.js
+++ src/components/AppWrapper/index.js
@@ -2,13 +2,12 @@ import { element } from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '@/styles/GlobalStyles';
 import { themeDefault } from '@/styles/theme';
-import { ToastProvider } from 'react-toast-notifications';
 
 export default function AppWrapper({ children }) {
   return (
     <ThemeProvider theme={themeDefault}>
       <GlobalStyles />
-      <ToastProvider>{children}</ToastProvider>
+      {children}
     </ThemeProvider>
   );
 }
